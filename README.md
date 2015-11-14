60c92acf80c6de3f02ef003319fb788fde76418a
veremey <veremey@i.ua>
2015-11-14 21:06:25 +0200

    done

---
+++ README.md
@@ -0,0 +1,23 @@
+#Alushta
+
+## Tehnologies
+
+* HTML
+* Sass
+* jQuery
+
+
+### Странички для просмотра:
+
+* [index.html](http://veremey.github.io/alushta/)
+
+## Contacts
+
+Skype: baxazar
+
+Email: [veremey4uk@gmail.com](mailto:veremey4uk@gmail.com)
+
+GitHub: [https://github.com/veremey](https://github.com/veremey)
+
+myCV:  [google drive](https://drive.google.com/open?id=1TK9mt61RCe0p68Jt_lBX8pRnAtXPieYcpJr0OF9VwT0)
+
--- js/common.js
+++ js/common.js
@@ -5,22 +5,4 @@ head.ready(function() {
 		return false;
 	});
 
-
-
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
-
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
-
-
 });
