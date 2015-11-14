1fc6aa40e4e33cfbde9984a034ec0e05342e4842
veremey <veremey@i.ua>
2015-11-14 21:15:10 +0200

    slideUp

--- js/common.js
+++ js/common.js
@@ -2,7 +2,12 @@ head.ready(function() {
 
 	$('.js-town').on('click', function() {
 		$('.js-town-open').slideToggle();
+		$('.js_popup').addClass('is_active')
 		return false;
 	});
-
+	// popup
+	$('.js_popup').on('click', function() {
+		$(this).removeClass('is_active');
+		$('.js-town-open').slideUp('300');
+	});
 });
