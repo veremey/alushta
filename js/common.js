b917f08697ed1416c008791c569bc89ae922d6a2
veremey <veremey@i.ua>
2015-11-14 04:41:55 +0200

    strt

---
+++ js/common.js
@@ -0,0 +1,26 @@
+head.ready(function() {
+
+	$('.js-town').on('click', function() {
+		$('.js-town-open').slideToggle();
+		return false;
+	});
+
+
+
+	// $(document).on("click", function(){
+	// 	$(".js-popup").hide();
+	// });
+
+	// function scrollFixedElements() {
+	//     var scroll_left = $(this).scrollLeft();
+	//     $(".fixed-element").css({
+	//         left: -scroll_left
+	//     });
+	// }
+	// scrollFixedElements();
+	// $(window).scroll(function(){
+	//     scrollFixedElements()
+	// });
+
+
+});
